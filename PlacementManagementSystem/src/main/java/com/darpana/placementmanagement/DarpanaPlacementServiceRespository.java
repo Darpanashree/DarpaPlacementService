49ab41dbfe8acff1cab51ce48a843df9b0171434
91636 <91636@INBOOK_X1_SLIM>
2024-09-26 20:10:58 +0530

    PlacementManagementSystem

---
+++ PlacementManagementSystem/src/main/java/com/darpana/placementmanagement/DarpanaPlacementServiceRespository.java
@@ -0,0 +1,8 @@
+package com.darpana.placementmanagement;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface DarpanaPlacementServiceRespository extends JpaRepository<DarpanaPlacement,Integer>{
+
+}
+
